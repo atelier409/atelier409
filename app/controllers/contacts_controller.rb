5a714dc13d592f7a8b7014049bc9970de59869e2
tanguy leveque <leveque.tanguy@outlook.fr>
2021-06-14 12:10:35 +0200

    contact

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,17 @@
+class ContactsController < ApplicationController
+def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      redirect_to root_path, notice: 'Merci, votre message a bien été envoyé. On vous contact au plus vite !.'
+    else
+      flash.now[:error] = 'Cannot send message'
+      render :new
+    end
+  end
+end
+
---
+++ app/models/contact.rb
@@ -0,0 +1,14 @@
+class Contact < ApplicationRecord
+attribute :name, validate: true
+  attribute :email, validate: true
+  attribute :message
+  def headers
+    {
+      #this is the subject for the email generated, it can be anything you want
+      subject: "Contact from Ateleir409",
+      to: 'leveque.tanguy@outlook.fr',
+      from: %("#{name}" <#{email}>)
+      #the from will display the name entered by the user followed by the email
+    }
+  end
+end
